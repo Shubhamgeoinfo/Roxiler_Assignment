b913ad7971b153bded622ea934b870aa63804a70
Shubham Kumar <54912405+Shubhamgeoinfo@users.noreply.github.com>
2024-03-24 16:14:28 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log({"conn":process.env.MONGO_URL})
     console.log(
       `Server is Conneted To Mongodb Databse and ready for CRUD operation ${conn.connection.host}`
     );
@@ -10,6 +11,7 @@ const connectDB = async () => {
     console.log(
       `Connection Error found  in db file during connection to Database ${error}`
     );
+    console.log({"conn":process.env.MONGO_URL})
   }
 };
 
